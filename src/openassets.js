669eb39cf664d7d0013b1f2207ec0c8c1a87daaa
Sebastian Kippe <sebastian@kip.pe>
2016-01-22 16:46:22 -0500

    Update metadata

--- src/openassets.js
+++ src/openassets.js
@@ -1,16 +1,6 @@
 // Description:
 //   Manage a openassets wallet and send rewards to people
 //
-// Configuration:
-//
-//   OA_ASSET_FROM_ADDRESS: sender's wallet open asset address
-//   OA_DEFAULT_QUANTITY: qunatity of assets that should be sent if not provided by the user
-//   OA_ASSET_ID: ID of the asset to be transferred
-//   OA_SERVER_URL: URL of the open assets server that does the actual transfer
-//   OA_SERVER_USERNAME: username for server basic auth
-//   OA_SERVER_PASSWORD: password for server basic auth
-//   OA_MAX_QUANTITY (optional): maximum quantity of assets that can be transferred
-//
 // Commands:
 //   kredits address add <nick> <address> - Add an Open Assets address for a user
 //   kredits address remove <nick> - Delete a user's addressbook entry
@@ -18,6 +8,16 @@
 //   kredits show <nick> - Show kredit balance of a user
 //   kredits send <amount> to <nick> - Send kredits to a user
 //   kredits list - Show list of all kredit holders
+//
+// Configuration:
+//   OA_BOT_KEYWORD: The keyword for issuing asset commands. Usually the asset name in lowercase, e.g. "kredits"
+//   OA_ASSET_ID: ID of the asset to be transferred
+//   OA_ASSET_FROM_ADDRESS: Sender's wallet open asset address
+//   OA_DEFAULT_QUANTITY: Quantity of assets that should be sent if not provided by the user
+//   OA_SERVER_URL: URL of the open assets server that does the actual transfer
+//   OA_SERVER_USERNAME: Username for server basic auth
+//   OA_SERVER_PASSWORD: Password for server basic auth
+//   OA_MAX_QUANTITY (optional): Maximum quantity of assets that can be transferred
 //
 // Authors:
 //   Michael Bumann <hello@michaelbumann.com>
